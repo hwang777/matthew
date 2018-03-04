177d9ef17070530ee7929b84660b7e8f005dd00e
hwang777 <hw2@hotmail.com>
2018-03-04 14:13:31 -0600

    first

---
+++ src/awesomeking888/Maindumb.java
@@ -0,0 +1,38 @@
+package awesomeking888;
+
+public class Maindumb {
+
+	public static void main(String[] args) {
+		System.out.println("oof");
+		boolean trueOfFalse = true;
+		System.out.println(trueOfFalse);
+		int x;
+		x = 888;
+		int oof;
+		oof = 1337;
+		int result = x + oof;
+		System.out.println(result);
+		System.out.println(x);
+		System.out.println(123);
+		System.out.println("Hi");
+		System.out.print("awesome");
+		System.out.print("king");
+		System.out.println(888);
+		System.out.println("https://www.youtube.com/watch?v=3ynJyURuUUM");
+		System.out.println("\tKevin The Dumb\n \tsays,\n \"We are idiots!\"");
+		int num = 888;
+		if(num % 2 == 1)
+		System.out.println("Odd number");
+		
+		
+		else{
+			System.out.println("Even number");
+		}
+		if(num > 444 && num == 888)
+			System.out.println("Greater than 444 and num equals 888");
+		else
+			System.out.println("Less than 444 but not 888");
+		System.out.println("LMAO I DE BEST U CANNOT BEAT MEH");
+	}
+
+	}
